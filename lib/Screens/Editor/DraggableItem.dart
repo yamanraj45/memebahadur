9e6a3fb10142bad830bfaea12594eb75ad8820e5
kaushu42 <kaushu42@gmail.com>
2020-08-08 18:09:13 +0545

    Fixed draggable text item

--- lib/Screens/Editor/DraggableItem.dart
+++ lib/Screens/Editor/DraggableItem.dart
@@ -8,18 +8,37 @@ class DraggableItem extends StatefulWidget {
   Offset position = Offset(0.0, 0.0);
 
   DraggableItem(this.initPos, this.label, this.widgetOffset,
-      {this.color = Colors.black});
+      {this.color = Colors.white});
 
   @override
   DraggableItemState createState() => DraggableItemState();
 }
 
 class DraggableItemState extends State<DraggableItem> {
-  String _text = "Hello";
+  final String _text = "Add your text here";
   @override
   void initState() {
     super.initState();
   }
+
+  var shadow = [
+    Shadow(
+        // bottomLeft
+        offset: Offset(-1.5, -1.5),
+        color: Colors.black),
+    Shadow(
+        // bottomRight
+        offset: Offset(1.5, -1.5),
+        color: Colors.black),
+    Shadow(
+        // topRight
+        offset: Offset(1.5, 1.5),
+        color: Colors.black),
+    Shadow(
+        // topLeft
+        offset: Offset(-1.5, 1.5),
+        color: Colors.black),
+  ];
 
   final myController = TextEditingController();
   @override
@@ -27,54 +46,50 @@ class DraggableItemState extends State<DraggableItem> {
     return Positioned(
       left: widget.position.dx,
       top: widget.position.dy,
-      child: Draggable(
-        child: Container(
-          // height: 100,
-          width: 100,
-          // color: Colors.red,
-          child: Center(
-            child: TextField(
-              controller: myController,
-              decoration: InputDecoration(
-                hintText: _text,
-                hintStyle: TextStyle(
+      child: Focus(
+        onFocusChange: (value) => {setState(() {})},
+        child: Draggable(
+          child: Container(
+            child: IntrinsicWidth(
+              child: TextField(
+                style: TextStyle(
+                  shadows: shadow,
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+                controller: myController,
+                decoration: InputDecoration(
+                  hintText: _text,
+                  hintStyle: TextStyle(
+                    shadows: shadow,
                     fontSize: 20.0,
                     color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    shadows: <Shadow>[
-                      Shadow(
-                        offset: Offset(2.0, 2.0),
-                        blurRadius: 3.0,
-                        color: Colors.black87,
-                      ),
-                    ]),
-                border: InputBorder.none,
+                    // fontWeight: FontWeight.bold,
+                  ),
+                  border: InputBorder.none,
+                ),
               ),
-              onChanged: (text) {
-                setState(() {
-                  print(_text);
-                  _text = myController.text;
-                });
-              },
             ),
           ),
-        ),
-        onDraggableCanceled: (velocity, offset) {
-          print(offset);
-          setState(() {
-            widget.position = offset - widget.widgetOffset;
-          });
-        },
-        maxSimultaneousDrags: 1,
-        childWhenDragging: Container(),
-        feedback: Container(
-          child: Center(
-            child: Text(
-              widget.label,
-              style: TextStyle(
-                color: widget.color,
-                decoration: TextDecoration.none,
-                fontSize: 18.0,
+          onDraggableCanceled: (velocity, offset) {
+            print(offset);
+            setState(() {
+              widget.position = offset - widget.widgetOffset;
+            });
+          },
+          maxSimultaneousDrags: 1,
+          childWhenDragging: Container(),
+          feedback: Container(
+            child: Center(
+              child: Text(
+                (myController.text == "") ? _text : myController.text,
+                style: TextStyle(
+                  shadows: shadow,
+                  color: Colors.white,
+                  decoration: TextDecoration.none,
+                  fontSize: 20.0,
+                ),
               ),
             ),
           ),
