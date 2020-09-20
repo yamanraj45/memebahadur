0e4caffb93b7fa98be2c3fa3cf93fa9589d25cac
yamanraj45 <yamanrajmishra99@gmail.com>
2020-09-20 08:28:59 +0545

    Basic Lyout Created Almost Complete

--- lib/Screens/SocialMedia/GoogleDidiYouMean.dart
+++ lib/Screens/SocialMedia/GoogleDidiYouMean.dart
@@ -1,140 +1,157 @@
 import 'package:flutter/material.dart';
-import 'package:memebahadur/widgets/MemeScaffold.dart';
 
 class DidYouMean extends StatefulWidget {
   @override
-  _DidYouMeanState createState() => _DidYouMeanState();
+  DidYouMeanState createState() => DidYouMeanState();
 }
 
-class _DidYouMeanState extends State<DidYouMean> {
-  String didyoumeantext = '';
+class DidYouMeanState extends State<DidYouMean>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  @override
+  void initState() {
+    _tabController = new TabController(length: 5, vsync: this);
+    super.initState();
+  }
+
+  String didyoumeantext;
   TextStyle category = TextStyle(color: Colors.grey, fontSize: 12);
   var _googlesearchtext = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return MemeScaffold(
-      child: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(8.0),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Icon(Icons.menu),
-                            Text('Google'),
-                            CircleAvatar(
-                              backgroundColor: Colors.red,
-                            )
-                          ],
-                        ),
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Icon(Icons.menu),
+                          Text('Google'),
+                          CircleAvatar(
+                            backgroundColor: Colors.red,
+                          )
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Expanded(
-                              child: Container(
-                                height: 50.0,
-                                child: TextField(
-                                    controller: _googlesearchtext,
-                                    decoration: InputDecoration(
-                                      fillColor: Colors.grey,
-                                      focusColor: Colors.grey,
-                                      hoverColor: Colors.grey,
-                                      border: OutlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(30.0),
-                                      ),
-                                      prefixIcon: Icon(Icons.search),
-                                      suffixIcon: _googlesearchtext.text.isEmpty
-                                          ? Icon(Icons.mic)
-                                          : IconButton(
-                                              icon: Icon(Icons.close),
-                                              onPressed: () =>
-                                                  _googlesearchtext.clear(),
-                                            ),
-                                    )),
-                              ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              height: 45.0,
+                              child: TextField(
+                                  controller: _googlesearchtext,
+                                  decoration: InputDecoration(
+                                    fillColor: Colors.grey,
+                                    focusColor: Colors.grey,
+                                    hoverColor: Colors.grey,
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30.0),
+                                    ),
+                                    prefixIcon: Icon(Icons.search),
+                                    suffixIcon: _googlesearchtext.text.isEmpty
+                                        ? Icon(Icons.mic)
+                                        : IconButton(
+                                            icon: Icon(Icons.close),
+                                            onPressed: () =>
+                                                _googlesearchtext.clear(),
+                                          ),
+                                  )),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 10),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Text(
-                              'ALL',
-                              style: category,
-                            ),
-                            Text(
-                              'IMAGES',
-                              style: category,
-                            ),
-                            Text(
-                              'BOOKS',
-                              style: category,
-                            ),
-                            Text(
-                              'VIDEOS',
-                              style: category,
-                            ),
-                            Text(
-                              'MAP',
-                              style: category,
-                            ),
-                          ],
-                        ),
+              ),
+              TabBar(
+                unselectedLabelColor: Colors.black,
+                labelColor: Colors.red,
+                tabs: [
+                  Tab(
+                    child: Text(
+                      'ALL',
+                      style: category,
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'IMAGES',
+                      style: TextStyle(fontSize: 11, color: Colors.grey),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'VIDEOS',
+                      style: category,
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'BOOKS',
+                      style: category,
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'MAPS',
+                      style: category,
+                    ),
+                  ),
+                ],
+                controller: _tabController,
+                indicatorSize: TabBarIndicatorSize.tab,
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    Text('people'),
+                    Text('Person'),
+                    Text('Person'),
+                    Text('Person'),
+                    Text('Person'),
+                  ],
+                  controller: _tabController,
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            'Did You mean: $didyoumeantext',
+                            style: TextStyle(fontSize: 16),
+                          )
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Divider(
-                  color: Colors.grey,
-                ),
-                Container(
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(10.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Text(
-                              'Did You mean: $didyoumeantext',
-                              style: TextStyle(fontSize: 16),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
