5a4d0fc9c1424b992275e3549a6102afae959d3c
kaushu42 <kaushu42@gmail.com>
2020-09-20 20:15:22 +0545

    [feat] Save and share feature added

--- lib/Screens/SocialMedia/GoogleTranslator.dart
+++ lib/Screens/SocialMedia/GoogleTranslator.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:esys_flutter_share/esys_flutter_share.dart';
 import 'package:flutter/material.dart';
+import 'package:memebahadur/utils/screenshot.dart';
 import 'package:memebahadur/widgets/InputText.dart';
 import 'package:memebahadur/widgets/MemeScaffold.dart';
 
@@ -8,148 +12,186 @@ class GoogleTranslator extends StatefulWidget {
 }
 
 class _GoogleTranslatorState extends State<GoogleTranslator> {
+  bool _isPageEdited = false;
   String langTranslatedFrom = 'Nepali';
   String langTranslatedTo = 'English';
   String translatedfrom = 'Enter Text';
   String translatedto = '';
+
+  final GlobalKey previewContainer = new GlobalKey();
   var _translatedfrom = TextEditingController();
   var _translatedto = TextEditingController();
+
+  _onBackPress() {
+    onBackPress(context, flag: _isPageEdited);
+  }
+
+  _onSavePress() {
+    onSavePress(context, previewContainer);
+  }
+
+  _onSharePress() async {
+    WidgetsBinding.instance.focusManager.primaryFocus?.unfocus();
+    Future.delayed(Duration(milliseconds: 100)).then((value) {
+      takeScreenshot(previewContainer).then((filename) {
+        Share.file("MemeBahadur", "memebahadur.jpg",
+            File(filename).readAsBytesSync(), "image/jpg");
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MemeScaffold(
+      onBackKeyPress: () {
+        _onBackPress();
+      },
+      onBackPress: _onBackPress,
+      onSavePress: _onSavePress,
+      onSharePress: _onSharePress,
       child: SafeArea(
         child: SingleChildScrollView(
           child: Container(
             padding: EdgeInsets.all(8.0),
             child: Column(
               children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
+                RepaintBoundary(
+                  key: previewContainer,
+                  child: Container(
+                    color: Colors.white,
+                    child: Column(
+                      children: [
                         Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    Container(
+                                      child: Text(
+                                        langTranslatedFrom,
+                                        style:
+                                            TextStyle(color: Colors.blue[600]),
+                                      ),
+                                    ),
+                                    Icon(Icons.arrow_drop_down)
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Container(
+                              height: 50,
+                              child: Icon(Icons.repeat),
+                            ),
                             Container(
                               child: Text(
-                                langTranslatedFrom,
+                                langTranslatedTo,
                                 style: TextStyle(color: Colors.blue[600]),
                               ),
                             ),
-                            Icon(Icons.arrow_drop_down)
                           ],
                         ),
-                      ],
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.repeat),
-                      onPressed: () {},
-                    ),
-                    Container(
-                      child: Text(
-                        langTranslatedTo,
-                        style: TextStyle(color: Colors.blue[600]),
-                      ),
-                    ),
-                  ],
-                ),
-                Divider(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(18.0),
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.7,
-                        child: Text(
-                          translatedfrom,
-                          style: TextStyle(fontSize: 25),
-                        ),
-                      ),
-                    ),
-                    _translatedto.text.isEmpty
-                        ? Container()
-                        : Container(
-                            alignment: Alignment.topRight,
-                            padding: const EdgeInsets.only(right: 8.0),
-                            child: IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  _translatedto.clear();
-                                  translatedfrom = 'Enter Text';
-                                });
-                              },
-                              icon: Icon(Icons.close),
-                            ),
-                          )
-                  ],
-                ),
-                _translatedto.text.isEmpty
-                    ? Container()
-                    : Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+                        Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
-                            Container(
-                              alignment: Alignment.topRight,
-                              child: Icon(
-                                Icons.volume_up,
-                                color: Colors.grey[700],
-                                size: 30,
+                            Padding(
+                              padding: const EdgeInsets.all(18.0),
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.7,
+                                child: Text(
+                                  translatedfrom,
+                                  style: TextStyle(fontSize: 25),
+                                ),
                               ),
                             ),
-                            Container(
-                              alignment: Alignment.topRight,
-                              child: Icon(
-                                Icons.mic,
-                                color: Colors.grey[700],
-                                size: 30,
-                              ),
-                            ),
+                            _translatedto.text.isEmpty
+                                ? Container()
+                                : Container(
+                                    alignment: Alignment.topRight,
+                                    padding: const EdgeInsets.only(right: 8.0),
+                                    child: IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _translatedto.clear();
+                                          translatedfrom = 'Enter Text';
+                                        });
+                                      },
+                                      icon: Icon(Icons.close),
+                                    ),
+                                  )
                           ],
                         ),
-                      ),
-                Divider(
-                  thickness: 1,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(18.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 1,
-                    child: Text(
-                      translatedto,
-                      style: TextStyle(fontSize: 25),
+                        _translatedto.text.isEmpty
+                            ? Container()
+                            : Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: <Widget>[
+                                    Container(
+                                      alignment: Alignment.topRight,
+                                      child: Icon(
+                                        Icons.volume_up,
+                                        color: Colors.grey[700],
+                                        size: 30,
+                                      ),
+                                    ),
+                                    Container(
+                                      alignment: Alignment.topRight,
+                                      child: Icon(
+                                        Icons.mic,
+                                        color: Colors.grey[700],
+                                        size: 30,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                        Divider(
+                          thickness: 1,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width * 1,
+                            child: Text(
+                              translatedto,
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          ),
+                        ),
+                        _translatedfrom.text.isEmpty
+                            ? Container()
+                            : Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: <Widget>[
+                                    Container(
+                                      alignment: Alignment.topRight,
+                                      child: Icon(
+                                        Icons.volume_up,
+                                        color: Colors.grey[700],
+                                        size: 30,
+                                      ),
+                                    ),
+                                    Container(
+                                      alignment: Alignment.topRight,
+                                      child: Icon(
+                                        Icons.content_copy,
+                                        color: Colors.grey[700],
+                                        size: 30,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                      ],
                     ),
                   ),
                 ),
-                _translatedfrom.text.isEmpty
-                    ? Container()
-                    : Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            Container(
-                              alignment: Alignment.topRight,
-                              child: Icon(
-                                Icons.volume_up,
-                                color: Colors.grey[700],
-                                size: 30,
-                              ),
-                            ),
-                            Container(
-                              alignment: Alignment.topRight,
-                              child: Icon(
-                                Icons.content_copy,
-                                color: Colors.grey[700],
-                                size: 30,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
                 Padding(
                   padding: EdgeInsets.all(8.0),
                 ),
@@ -159,7 +201,7 @@ class _GoogleTranslatorState extends State<GoogleTranslator> {
                       Container(
                         child: InputText(
                           maxLength: 20,
-                          label: 'Language To Translate From',
+                          label: 'Translate From',
                           onChanged: (value) {
                             setState(() {
                               langTranslatedFrom = value;
@@ -173,7 +215,7 @@ class _GoogleTranslatorState extends State<GoogleTranslator> {
                       Container(
                         child: InputText(
                           maxLength: 20,
-                          label: 'Language To Translate',
+                          label: 'Translate',
                           onChanged: (value) {
                             setState(() {
                               langTranslatedTo = value;
